1e4e0837b0c39b85388677ce48c1f97b095ea05e
regine <regine.issan@gmail.com>
2024-12-20 16:49:55 +0200

    Loan API:  add dependency injection and  CORSs upport

--- HomeAssignment.Abra/LoansAPI/Program.cs
+++ HomeAssignment.Abra/LoansAPI/Program.cs
@@ -1,4 +1,24 @@
+using LoansLib.BL.Interfaces;
+using LoansLib.BL.Services;
+using LoansLib.Data;
+using LoansLib.Data.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+//My added to fix Cors. As a result server send in reponse this header: access-control-allow-origin:
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        //policy.WithOrigins("http://localhost:3000/") //react app
+        policy.AllowAnyOrigin() //Temp: any react app
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
+
 
 // Add services to the container.
 
@@ -7,7 +27,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+
+//My Register services
+//note: AddControllers handle the logging
+builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
+builder.Services.AddSingleton<ILoanStrategyFactory, LoanStrategyFactory>();
+builder.Services.AddSingleton<ICustomerService, CustomerService>();
+builder.Services.AddSingleton<ILoanService, LoanService>();
+
+
+
 var app = builder.Build();
+
+
+//MY
+app.UseCors("AllowReactApp");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
